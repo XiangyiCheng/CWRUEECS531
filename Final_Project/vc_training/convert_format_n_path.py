08729af3580ca49a9f71a9a8e4a6d6a20843465e
XiangyiCheng <xxc283@case.edu>
2018-04-27 14:51:03 -0400

    final project in process

--- Final_Project/vc_training/convert_format_n_path.py
+++ Final_Project/vc_training/convert_format_n_path.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def convert_pos():
     pic_num=1
-    for img in os.listdir('raw_images/selected_pos_image'):
+    for img in os.listdir('vc_training/raw_images/selected_pos_image'):
         try:
-            img=cv2.imread('raw_images/selected_pos_image/'+str(img),cv2.IMREAD_GRAYSCALE)
+            img=cv2.imread('vc_training/raw_images/selected_pos_image/'+str(img),cv2.IMREAD_GRAYSCALE)
             resized_img=cv2.resize(img,(50,50))
             cv2.imwrite('pos/'+str(pic_num)+'.jpg',resized_img)
             pic_num+=1
@@ -15,9 +15,9 @@ def convert_pos():
 
 def convert_neg():
     pic_num=1173
-    for img in os.listdir('new_neg'):
+    for img in os.listdir('vc_training/new_neg'):
         try:
-            img=cv2.imread('new_neg/'+str(img),cv2.IMREAD_GRAYSCALE)
+            img=cv2.imread('vc_training/new_neg/'+str(img),cv2.IMREAD_GRAYSCALE)
             resized_img=cv2.resize(img,(50,50))
             cv2.imwrite('neg/'+str(pic_num)+'.jpg',resized_img)
             pic_num+=1
